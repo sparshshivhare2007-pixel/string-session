66353c4f45077045f6508862d61f065345d4c47d
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-12-18 16:42:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 12380656))
-API_HASH = getenv("API_HASH", "d927c13beaaf5110f25c505b7c071273")
+API_ID = int(getenv("API_ID", 26658182))
+API_HASH = getenv("API_HASH", "d3cdbdb3b81014c71ec60ed03d2b4d8f")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", 6399386263))
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://yash:shivanshudeo@yk.6bvcjqp.mongodb.net/?retryWrites=true&w=majority&appName=yk")
-MUST_JOIN = getenv("MUST_JOIN", "akaChampu")
+OWNER_ID = int(getenv("OWNER_ID", 8353151202))
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://pusers:nycreation@nycreation.pd4klp1.mongodb.net/?retryWrites=true&w=majority&appName=NYCREATION")
+MUST_JOIN = getenv("MUST_JOIN", "destinybots")
